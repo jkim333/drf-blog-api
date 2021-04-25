f1191c92bfaaa5bfdea2c61fb334009116523ad8
Jihyung Kim <kimjihyung3@gmail.com>
2021-04-25 10:48:27 +1000

    Update views and urls to viewsets and routers

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,9 @@
 from rest_framework import generics, permissions
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 from .permissions import IsAuthorOrReadOnly
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
@@ -11,3 +13,20 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+class UserList(generics.ListCreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
